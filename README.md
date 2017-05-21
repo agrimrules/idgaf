cee4423cb952cbdbbbd1b5bc307a7a5006680be0
Agrim Asthana <agrimasthana@gmail.com>
2017-05-21 01:02:20 -0500

    making good progress in the right direction

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#idgaf
+# idgaf    
 A very basic extension. with lots of hope, potential and calibre.   
--- idgaf.js
+++ idgaf.js
@@ -1,3 +1,15 @@
-var logo = document.querySelector('.fb_logo');
-console.log(logo);
-logo.style.visibility = "hidden";
+var bannedTerms = ["marriage", "wedding", "hitched", "pelli", "shaadi"];
+function cleanup(){
+  console.log("Running Cleanup")
+  var posts = document.getElementsByClassName("_5jmm");
+  for(i=0; i < posts.length ; i++){
+   var paragh =  posts[i].getElementsByTagName("p");
+   for (j=0; j < paragh.length; j++){
+     txt = paragh[j].textContent.toLowerCase()
+     if (txt.indexOf("marriage") !== -1 || txt.indexOf("wedding") !== -1){
+       posts[i].remove();
+     }
+   }
+  }
+}
+document.addEventListener("scroll", cleanup);
